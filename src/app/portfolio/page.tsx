368690c2246baf648dee2aaf5c25368831b357a5
SojiJoro <71898347+SojiJoro@users.noreply.github.com>
2025-03-24 18:30:53 +0000

    page update

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -1,10 +1,69 @@
-export default function Portfolio() {
-    return (
-      <main className="p-8">
-        <h1 className="text-3xl font-bold">Our Portfolio</h1>
-        <p className="mt-4">Check out our latest projects and designs.</p>
-        {/* Add portfolio items here */}
-      </main>
-    );
-  }
-  
+"use client";
+import Link from "next/link";
+import Image from "next/image";
+
+export default function PortfolioPage() {
+  return (
+    <>
+      {/* PORTFOLIO SECTION */}
+      <div className="container py-16 md:py-20" id="portfolio">
+        <h2 className="text-center font-header text-4xl font-semibold uppercase text-primary sm:text-5xl lg:text-6xl">
+          Check out my Portfolio
+        </h2>
+        <h3 className="pt-6 text-center font-header text-xl font-medium text-black sm:text-2xl lg:text-3xl">
+          Here&apos;s what I have done in the past
+        </h3>
+        <div className="mx-auto grid w-full grid-cols-1 gap-8 pt-12 sm:w-3/4 md:gap-10 lg:w-full lg:grid-cols-2">
+          <Link
+            href="/"
+            className="mx-auto transform transition-all hover:scale-105 md:mx-0"
+          >
+            <Image
+              src="/images/portfolio-apple.jpeg"
+              alt="portfolio"
+              width={800}
+              height={600}
+              className="w-full shadow"
+            />
+          </Link>
+          <Link
+            href="/"
+            className="mx-auto transform transition-all hover:scale-105 md:mx-0"
+          >
+            <Image
+              src="/images/portfolio-stripe.jpeg"
+              alt="portfolio"
+              width={800}
+              height={600}
+              className="w-full shadow"
+            />
+          </Link>
+          <Link
+            href="/"
+            className="mx-auto transform transition-all hover:scale-105 md:mx-0"
+          >
+            <Image
+              src="/images/portfolio-fedex.jpeg"
+              alt="portfolio"
+              width={800}
+              height={600}
+              className="w-full shadow"
+            />
+          </Link>
+          <Link
+            href="/"
+            className="mx-auto transform transition-all hover:scale-105 md:mx-0"
+          >
+            <Image
+              src="/images/portfolio-microsoft.jpeg"
+              alt="portfolio"
+              width={800}
+              height={600}
+              className="w-full shadow"
+            />
+          </Link>
+        </div>
+      </div>
+    </>
+  );
+}
--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -1,10 +1,195 @@
-export default function Services() {
-    return (
-      <main className="p-8">
-        <h1 className="text-3xl font-bold">Our Services</h1>
-        <p className="mt-4">We provide top-notch services tailored to meet your needs.</p>
-        {/* Add more content related to services */}
-      </main>
-    );
-  }
-  
+"use client";
+import Image from "next/image";
+
+export default function ServicesPage() {
+  return (
+    <>
+      {/* SERVICES SECTION */}
+      <div className="container py-16 md:py-20" id="services">
+        <h2 className="text-center font-header text-4xl font-semibold uppercase text-primary sm:text-5xl lg:text-6xl">
+          Here&apos;s what I&apos;m good at
+        </h2>
+        <h3 className="pt-6 text-center font-header text-xl font-medium text-black sm:text-2xl lg:text-3xl">
+          These are the services I offer
+        </h3>
+
+        <div className="grid grid-cols-1 gap-6 pt-10 sm:grid-cols-2 md:gap-10 md:pt-12 lg:grid-cols-3">
+          {/* Service 1 */}
+          <div className="group rounded px-8 py-12 shadow hover:bg-primary">
+            <div className="mx-auto h-24 w-24 text-center xl:h-28 xl:w-28">
+              <div className="block group-hover:hidden">
+                <Image
+                  src="/images/icon-development-black.svg"
+                  alt="development icon"
+                  width={96}
+                  height={96}
+                />
+              </div>
+              <div className="hidden group-hover:block">
+                <Image
+                  src="/images/icon-development-white.svg"
+                  alt="development icon"
+                  width={96}
+                  height={96}
+                />
+              </div>
+            </div>
+            <div className="text-center">
+              <h3 className="pt-8 text-lg font-semibold uppercase text-primary group-hover:text-yellow lg:text-xl">
+                WEB DEVELOPMENT
+              </h3>
+              <p className="text-grey pt-4 text-sm group-hover:text-white md:text-base">
+                Creating modern, responsive websites that are visually appealing.
+              </p>
+            </div>
+          </div>
+          {/* Service 2 */}
+          <div className="group rounded px-8 py-12 shadow hover:bg-primary">
+            <div className="mx-auto h-24 w-24 text-center xl:h-28 xl:w-28">
+              <div className="block group-hover:hidden">
+                <Image
+                  src="/images/icon-content-black.svg"
+                  alt="content marketing icon"
+                  width={96}
+                  height={96}
+                />
+              </div>
+              <div className="hidden group-hover:block">
+                <Image
+                  src="/images/icon-content-white.svg"
+                  alt="content marketing icon"
+                  width={96}
+                  height={96}
+                />
+              </div>
+            </div>
+            <div className="text-center">
+              <h3 className="pt-8 text-lg font-semibold uppercase text-primary group-hover:text-yellow lg:text-xl">
+                Content Creation
+              </h3>
+              <p className="text-grey pt-4 text-sm group-hover:text-white md:text-base">
+                Developing engaging visuals and copy for social media.
+              </p>
+            </div>
+          </div>
+          {/* Service 3 */}
+          <div className="group rounded px-8 py-12 shadow hover:bg-primary">
+            <div className="mx-auto h-24 w-24 text-center xl:h-28 xl:w-28">
+              <div className="block group-hover:hidden">
+                <Image
+                  src="/images/icon-mobile-black.svg"
+                  alt="Mobile dev icon"
+                  width={96}
+                  height={96}
+                />
+              </div>
+              <div className="hidden group-hover:block">
+                <Image
+                  src="/images/icon-mobile-white.svg"
+                  alt="Mobile dev icon"
+                  width={96}
+                  height={96}
+                />
+              </div>
+            </div>
+            <div className="text-center">
+              <h3 className="pt-8 text-lg font-semibold uppercase text-primary group-hover:text-yellow lg:text-xl">
+                Mobile Development
+              </h3>
+              <p className="text-grey pt-4 text-sm group-hover:text-white md:text-base">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              </p>
+            </div>
+          </div>
+          {/* Service 4 */}
+          <div className="group rounded px-8 py-12 shadow hover:bg-primary">
+            <div className="mx-auto h-24 w-24 text-center xl:h-28 xl:w-28">
+              <div className="block group-hover:hidden">
+                <Image
+                  src="/images/icon-email-black.svg"
+                  alt="Email Marketing icon"
+                  width={96}
+                  height={96}
+                />
+              </div>
+              <div className="hidden group-hover:block">
+                <Image
+                  src="/images/icon-email-white.svg"
+                  alt="Email Marketing icon"
+                  width={96}
+                  height={96}
+                />
+              </div>
+            </div>
+            <div className="text-center">
+              <h3 className="pt-8 text-lg font-semibold uppercase text-primary group-hover:text-yellow lg:text-xl">
+                Email Development
+              </h3>
+              <p className="text-grey pt-4 text-sm group-hover:text-white md:text-base">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              </p>
+            </div>
+          </div>
+          {/* Service 5 */}
+          <div className="group rounded px-8 py-12 shadow hover:bg-primary">
+            <div className="mx-auto h-24 w-24 text-center xl:h-28 xl:w-28">
+              <div className="block group-hover:hidden">
+                <Image
+                  src="/images/icon-design-black.svg"
+                  alt="Theme Design icon"
+                  width={96}
+                  height={96}
+                />
+              </div>
+              <div className="hidden group-hover:block">
+                <Image
+                  src="/images/icon-design-white.svg"
+                  alt="Theme Design icon"
+                  width={96}
+                  height={96}
+                />
+              </div>
+            </div>
+            <div className="text-center">
+              <h3 className="pt-8 text-lg font-semibold uppercase text-primary group-hover:text-yellow lg:text-xl">
+                Graphic Design
+              </h3>
+              <p className="text-grey pt-4 text-sm group-hover:text-white md:text-base">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              </p>
+            </div>
+          </div>
+          {/* Service 6 */}
+          <div className="group rounded px-8 py-12 shadow hover:bg-primary">
+            <div className="mx-auto h-24 w-24 text-center xl:h-28 xl:w-28">
+              <div className="block group-hover:hidden">
+                <Image
+                  src="/images/icon-graphics-black.svg"
+                  alt="Graphic Design icon"
+                  width={96}
+                  height={96}
+                />
+              </div>
+              <div className="hidden group-hover:block">
+                <Image
+                  src="/images/icon-graphics-white.svg"
+                  alt="Graphic Design icon"
+                  width={96}
+                  height={96}
+                />
+              </div>
+            </div>
+            <div className="text-center">
+              <h3 className="pt-8 text-lg font-semibold uppercase text-primary group-hover:text-yellow lg:text-xl">
+                Web Design
+              </h3>
+              <p className="text-grey pt-4 text-sm group-hover:text-white md:text-base">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
